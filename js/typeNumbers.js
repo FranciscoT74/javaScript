cbb6047f9f60249db6922b548f9c133bee8858a4
franciscoT74 <franciscotorres1412@gmail.com>
2021-10-05 20:04:52 -0500

    update jav

---
+++ js/typeNumbers.js
@@ -0,0 +1,12 @@
+tipoNumeros.js
+var miPrimeraVariable = "Hola a todos";
+/*
+NaN - Not-A-Number
+*/
+console.log(isNaN(miPrimeraVariable));
+miPrimeraVariable = 5;
+console.log(isNaN(miPrimeraVariable));
+//Maximo numero en JS
+console.log(Number.MAX_VALUE);
+//Minimo numero en JS
+console.log(Number.MIN_VALUE);
